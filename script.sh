5ad77573e597ece4afd43b08ac7d2b25eb23ccf3
Digvijay1133 <112845896+Digvijay1133@users.noreply.github.com>
2022-12-15 15:58:58 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 sudo yum update -y
 sudo yum -y install python3-pip
 git clone https://github.com/Digvijay1133/DEPLOY-FLASK-PYTHON-WEB-APPLICATION-.git
-cd flask-library-app/
+cd DEPLOY-FLASK-PYTHON-WEB-APPLICATION-/
 pip3 install -r requirements.txt
 python3 app.py
