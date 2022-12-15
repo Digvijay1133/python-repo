dad64ee3204255d09e057f2e581ecbd179836bea
Digvijay1133 <112845896+Digvijay1133@users.noreply.github.com>
2022-12-15 16:52:51 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 sudo yum update -y
 sudo yum -y install python3-pip
-git clone https://github.com/Digvijay1133/task.git
-cd task
+git clone https://github.com/Digvijay1133/deploy-python-web-application.git
+cd deploy-python-web-application
 pip3 install -r requirements.txt
 python3 app.py
