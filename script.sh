660191f0cceaa95948c016ea4f8dd46f579299a7
Digvijay1133 <112845896+Digvijay1133@users.noreply.github.com>
2022-12-19 17:01:32 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
-yum update -y
-yum -y install python3-pip
+ 
+ install python3-pip
 git clone https://github.com/Digvijay1133/deploy-python-web-application.git
 deploy-python-web-application
 pip3 install -r requirements.txt
